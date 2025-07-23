2e6f0ee6900f01639b4b2bc7940cb3838436cea8
NipunaLankara <nipunalankara@gmail.com>
2025-07-23 23:28:09 +0530

    admin1.1

---
+++ src/main/java/servlet/pahanaedu/servlet/admin/DeleteUsers.java
@@ -0,0 +1,26 @@
+package servlet.pahanaedu.servlet.admin;
+
+import servlet.pahanaedu.service.UserService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+
+@WebServlet("/admin/deleteUser")
+public class DeleteUsers extends HttpServlet {
+    private UserService userService = new UserService();
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int userId = Integer.parseInt(request.getParameter("id"));
+        try {
+            userService.deleteUserById(userId);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        response.sendRedirect("admin/customers");
+    }
+}
